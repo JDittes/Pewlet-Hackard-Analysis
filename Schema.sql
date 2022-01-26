f8247b68d1cee4648a0040233cb3400665f5e6d2
James Dittes <james.dittes@gmail.com>
2022-01-25 18:07:14 -0600

    added queries, titles is OK now

--- Schema.sql
+++ Schema.sql
@@ -48,9 +48,7 @@ CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-PRIMARY KEY (emp_no)
+	to_date DATE NOT NULL
 );
 
-SELECT * FROM titles;
+DROP TABLE titles;
---
+++ queries.sql
@@ -0,0 +1,84 @@
+--Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM dept_manager;
+
+--Just get a count
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM dept_manager;
+
+SELECT MAX(emp_no) FROM employees;
+SELECT nextval(emp_no);
+
+SELECT PG_GET_SERIAL_SEQUENCE('"employees"', 'emp_no');
+
+SELECT CURRVAL(PG_GET_SERIAL_SEQUENCE('"employees"', 'emp_no')) AS "Current Value", MAX("emp_no") AS "Max Value" FROM "employees";
+
+SELECT SETVAL((SELECT PG_GET_SERIAL_SEQUENCE('"employees"', 'emp_no')), (SELECT (MAX("emp_no") + 1) FROM "employees"), FALSE);
+
+SELECT MAX(emp_no) FROM employees;
+SELECT nextval('emp_no_sequence');
+
+--Retirement eligibility, saving to a doc
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info;
+
+DROP TABLE retirement_info;
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+-- Joining retirement_info and dept_emp tables with shortened names
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+SELECT ri.emp_no,
+    ri.first_name,
+    ri.last_name,
+de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
