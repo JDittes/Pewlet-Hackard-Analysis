2fcbd2753b8ffc0f5ecf7de6186de13722bf16b2
James Dittes <james.dittes@gmail.com>
2022-01-27 16:09:46 -0600

    Deliverable 1 2nd draft

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -7,5 +7,23 @@ ON (e.emp_no = ti.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY emp_no;
 
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO distinct_onployees
+FROM most_recent_title
+WHERE to_date = '9999-01-01'
+ORDER BY emp_no, to_date DESC;
 
-SELECT * FROM most_recent_title;
+SELECT * FROM most_recent_title
+
+--Retrieve the number of titles from Unique Titles table
+SELECT COUNT(*), title
+INTO retiring_titles
+FROM distinct_onployees
+GROUP BY title 
+ORDER BY count DESC;
+--cOUNT IS 72,457
+--Is this the right table?
